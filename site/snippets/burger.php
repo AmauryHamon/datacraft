2f385032a655700cfa58bba3a8ad12c30bfe281e
Amaury Hamon <contact@amauryhamon.com>
2025-03-08 17:14:10 +0100

    icons

---
+++ site/snippets/burger.php
@@ -0,0 +1,5 @@
+<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="8" y="8.5" width="24" height="1" fill="black"/>
+<rect x="8" y="19.5" width="24" height="1" fill="black"/>
+<rect x="8" y="30.5" width="24" height="1" fill="black"/>
+</svg>
