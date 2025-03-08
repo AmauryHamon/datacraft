4abeabf95b81bbedfbadf4f358871e80778642aa
Amaury Hamon <contact@amauryhamon.com>
2025-03-08 17:13:33 +0100

    matrix axis

---
+++ site/snippets/axis.php
@@ -0,0 +1,4 @@
+<div class="z-20 pointer-events-none fixed inset-0 w-full h-screen flex items-center justify-center">
+    <hr class="absolute w-full h-px border-lightgreen">
+    <hr class="absolute h-full w-px bg-lightgreen"> 
+</div>
