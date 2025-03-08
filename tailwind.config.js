2a7a5f8c2de2d8a96a088bd433b32315e1f1ed4f
Amaury Hamon <contact@amauryhamon.com>
2025-03-08 17:11:39 +0100

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/js/*.js", "./site/**/*.php", "./content/**/*.txt"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green': '#00ff00',
+        'lightgreen': '#80F980',
+        'ultralightgreen': '#CAFFCA',
+        'lightgrey': '#f3f3f3',
+      },
+      fontFamily: {
+        display: 'Windsor',
+        body: 'Gerrit-M',
+      },
+      dropShadow: {
+        'logo': '0 0 15px #00ff00'
+      },
+      backgroundSize: {
+        'size-200': '100% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 70%',
+        'pos-100': '0% 50%',
+      },
+
+
+    },
   },
   plugins: [],
 }
