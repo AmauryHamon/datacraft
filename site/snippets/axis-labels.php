8c356db66655fcf7239c1b4d93f33b9769dc50cd
Amaury Hamon <contact@amauryhamon.com>
2025-03-08 17:14:01 +0100

    axis labels

---
+++ site/snippets/axis-labels.php
@@ -0,0 +1,4 @@
+<div class="z-30 text-xs select-none fixed top-20 left-1/2 -translate-x-1/2">Emancipation</div>
+<div class="z-30 text-xs select-none fixed bottom-5 left-1/2 -translate-x-1/2">Productivity</div>
+<div class="z-30 text-xs select-none fixed top-1/2 left-5 -translate-y-1/2">Accessibility</div>
+<div class="z-30 text-xs select-none fixed top-1/2 right-5 -translate-y-1/2">Effort</div>
