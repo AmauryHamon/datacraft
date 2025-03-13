eb8053cb34d79661914cf10e7429bcb28dcea8d8
Amaury Hamon <contact@amauryhamon.com>
2025-03-13 14:58:33 +0100

    deactivate map temp

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -16,6 +16,9 @@
             </button>
         </nav>
     </div>
+    <div class="opacity-20 pointer-events-none">
+        <?php snippet('logo', ['showParagraph' => false])?>
+    </div>
     <div 
         class="fixed inset-0 w-full h-screen z-40 bg-lightgrey flex"
         x-show="menuOpen" 
@@ -26,11 +29,12 @@
         x-transition:leave-start="opacity-100 scale-100"
         x-transition:leave-end="opacity-0 scale-100"
     >
+        <?php snippet('axis')?>
         <ul class="w-full grid grid-cols-1 sm:grid-cols-2 max-h-[90vh] my-[5vh] sm:max-h-none place-content-center sm:place-content-around justify-items-stretch sm:content-stretch text-3xl sm:text-4xl leading-[1]">
-            <li class="flex justify-center items-center"><a class="py-4 sm:py-0 flex text-center justify-center items-center w-full h-full" href="">Map</a></li>
             <li class="flex justify-center items-center"><a class="py-4 sm:py-0 flex text-center justify-center items-center w-full h-full" href="/about">About</a></li>
+            <li class="flex justify-center items-center"><a class="py-4 sm:py-0 flex text-center justify-center items-center w-full h-full opacity-20 pointer-events-none" href="">Map</a></li>
             <li class="flex justify-center items-center"><a class="py-4 sm:py-0 flex text-center justify-center items-center w-full h-full opacity-20 pointer-events-none" href="">Cookbook (soon)</a></li>
-            <li class="flex justify-center items-center"><a class="py-4 sm:py-0 flex text-center justify-center items-center w-full h-full" href="">Contact</a></li>
+            <!-- <li class="flex justify-center items-center"><a class="py-4 sm:py-0 flex text-center justify-center items-center w-full h-full" href="">Contact</a></li> -->
         </ul>
         <div class="fixed left-0 bottom-0 z-50 w-full">
             <?php snippet('footer')?>
