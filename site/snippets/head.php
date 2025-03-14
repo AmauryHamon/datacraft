2033b5dd71a84d8dc77b349c9e88d7ab9170c9b1
Amaury Hamon <contact@amauryhamon.com>
2025-03-14 14:50:05 +0100

    bg change modal

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -6,7 +6,7 @@
     <title>datacraft</title>
     <?= css('assets/css/style.css') ?>
 </head>
-<body x-data="site">
+<body x-data="site" :class="modal ? 'bg-ultralightgreen' : 'bg-lightgrey'">
 
 <?php snippet('nav')?>
 <main class="min-h-screen">
