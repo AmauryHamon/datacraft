2cf57791c930151709ab312045b8c58d041affae
Amaury Hamon <contact@amauryhamon.com>
2025-03-14 14:48:22 +0100

    svg

--- site/snippets/cross.php
+++ site/snippets/cross.php
@@ -1,4 +1,4 @@
 <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-<line x1="8.35355" y1="8.64645" x2="32.3536" y2="32.6464" stroke="black"/>
-<line y1="-0.5" x2="33.9411" y2="-0.5" transform="matrix(-0.707107 0.707107 0.707107 0.707107 32 9)" stroke="black"/>
+<line x1="8.35355" y1="8.64645" x2="32.3536" y2="32.6464" stroke="currentcolor"/>
+<line y1="-0.5" x2="33.9411" y2="-0.5" transform="matrix(-0.707107 0.707107 0.707107 0.707107 32 9)" stroke="currentcolor"/>
 </svg>
