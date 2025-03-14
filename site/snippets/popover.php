225d63e3182c687cd49b4b5fa509084992f02753
Amaury Hamon <contact@amauryhamon.com>
2025-03-14 14:50:11 +0100

    popover wip

---
+++ site/snippets/popover.php
@@ -0,0 +1,19 @@
+<div
+    x-show="popover"
+    class="absolute p-2 min-w-96 top-10  bg-white text-black shadow-md rounded-md transition-opacity duration-300 flex gap-2"
+    x-transition:enter="transition ease-out delay-500 duration-150"
+    x-transition:enter-start="opacity-0 scale-100"
+    x-transition:enter-end="opacity-100 scale-100"
+    x-transition:leave="transition ease-in duration-300"
+    x-transition:leave-start="opacity-100 scale-100"
+    x-transition:leave-end="opacity-0 scale-100"
+    >
+    <div class="flex w-48">
+        <img class=" object-cover aspect-square" :src="node.images" alt="">
+    </div>
+    <div class="p-2 flex flex-col justify-between">
+        <h4 class="text-2xl/ultratight" x-text="node.title"></h4>
+        <div class="text-base flex flex-wrap gap-2"><span x-text="node.authors"></span><span x-text="node.date"></span></div>
+        <div class="flex flex-wrap text-xs gap-1"><span class="cardtag" x-text="node.category"></span><span class="cardtag" x-text="node.concept"></span><span class="cardtag" x-text="node.tools"></span></div>
+    </div>
+</div>
