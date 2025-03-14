f9481c807902daf4f86ba6eb400e5bed1646143e
Amaury Hamon <contact@amauryhamon.com>
2025-03-14 14:48:32 +0100

    alpine nodes

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,35 @@ window.Alpine = Alpine
 
 
 Alpine.data("site", () => ({
+    modal: false,
     init(){
         console.log("site init")
+    }
+}))
+Alpine.data("map", () => ({
+    filterOpen: false,
+    
+    nodes: [],
+    selectedNode: {},
+
+    init(){
+        this.fetchNodes()
+        console.log("map init")
+
+    },
+    async fetchNodes(){
+        try {
+            let response = await fetch('/map.json'); // Adjust path as needed
+            this.nodes = await response.json();
+            console.log("Nodes loaded:", this.nodes);
+            
+        } catch (error) {
+            console.error("Error loading nodes:", error);
+        }
+    },
+    openModal(node) {
+        this.selectedNode = node;
+        this.modal = true;
     }
 }))
 
