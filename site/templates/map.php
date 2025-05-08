870f1e8654339d3e36267a4bfcdc75381990ef87
Amaury Hamon <contact@amauryhamon.com>
2025-05-08 07:59:55 +0200

    WIP new filters + apparition cool

--- site/templates/map.php
+++ site/templates/map.php
@@ -2,16 +2,27 @@
 <?php snippet('axis') ?>
 <?php snippet('axis-labels') ?>
 <div x-data="map" x-init="init()" :class="modal ? 'bg-lightgreen' : 'bg-lightgrey'">
-    <button class="fixed z-50 top-5 left-1/2 -translate-x-1/2" @click="filterOpen = !filterOpen">Filters</button>
-    <?php snippet('filters') ?>
+    <!-- <button class="fixed z-50 top-5 left-1/2 -translate-x-1/2" @click="filterOpen = !filterOpen">Filters</button> -->
+    <div class="fixed z-50 top-10 left-1/2 -translate-x-1/2">
+        <ul class="flex gap-4">
+            <button>Projects</button>
+            <button>Legacy</button>
+            <button>Recipes</button>
+            <button class="">…</button>
+        </ul>
+    </div>
+    
     <?php if ($page->children()->isNotEmpty()): ?>
         <div class="fixed inset-0 w-full h-screen z-40">
-            <template x-for="node in nodes" :key="node.title">
+            <template x-for="(node, index) in nodes" :key="node.title">
                 <a
                     href=""
-                    class="select-none absolute hover:z-[100] flex flex-col justify-center items-center group drop-shadow-logo"
+                    
+                    x-data="{ message: '○', popover: false, visible: false }"
+                    x-init="setTimeout(()=>visible = true, index * 50)"
+                    :class="visible ? 'opacity-100' : 'opacity-0'"
+                    class="select-none absolute hover:z-[100] flex flex-col justify-center items-center group drop-shadow-logo transition-all duration-700 ease-out"
                     :style="'top: ' + node.ypos + '%; left: ' + node.xpos + '%;'"
-                    x-data="{ message: '○', popover: false }"
                     @mouseenter.prevent="message = '●'; popover = true"
                     @mouseleave.prevent="message = '○'; popover = false"
                     @click.prevent="openModal(node)">
