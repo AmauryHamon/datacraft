557a3d48711b593e6c6c85f38339965b299ccde0
Amaury Hamon <contact@amauryhamon.com>
2025-05-08 15:19:43 +0200

    fix logo

--- site/snippets/logo.php
+++ site/snippets/logo.php
@@ -1,7 +1,7 @@
 <div class="fixed z-30 inset-0 w-full h-screen flex flex-col gap-4 items-center justify-center">
-    <h1 class="text-lightgreen text-[15vw] font-display select-none drop-shadow-logo leading-[0.7]">datacraft</h1>
+    <h1 class="text-lightgreen text-[15vw] font-display select-none drop-shadow-logo leading-[0.7] mb-8">datacraft</h1>
     <?php if ($showParagraph ?? false): ?>
-        <p class="text-center text-xs sm:text-base">
+        <p class="mt-16 absolute top-1/2 text-center text-xs sm:text-base">
             <?=$site->tagline()?>
         </p>
     <?php endif; ?>
