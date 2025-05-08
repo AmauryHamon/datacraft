ac959b99c5a4cba1156420c93be3f45f75316e22
Amaury Hamon <contact@amauryhamon.com>
2025-05-08 15:33:03 +0200

    spa links

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,71 @@
 import Alpine from 'alpinejs'
- 
+import router from "@shaun/alpinejs-router"; 
 window.Alpine = Alpine
+Alpine.plugin(router);
 
 
 Alpine.data("site", () => ({
-    modal: false,
-    selectedNodeSlug: null,
-    init(){
-        this.$el.addEventListener('open-modal', e => {
-            this.modal = true
-            this.selectedNodeSlug = e.detail.node; // Set the selected node to be used in the modal
-            history.pushState(null, '', '?node=' + this.selectedNodeSlug)
-        })
-    }
-}))
-Alpine.data("map", () => ({
-    filterOpen: false,
-    
-    nodes: [],
-    selectedNode: {},
-
-    init(){
-        
-        
-
-
-    },
-    async fetchNodes(){
-        try {
-            let response = await fetch('/map.json'); // Adjust path as needed
-            this.nodes = await response.json();
-            console.log("Nodes loaded:", this.nodes);
-            
-        } catch (error) {
-            console.error("Error loading nodes:", error);
+    subpage: null,
+    subpagedata: null,
+    init() {
+        let pathname = window.location.pathname;
+        let paths = pathname.split("/");
+        if (paths.length > 1 && paths[1]) {
+          if (paths.length > 2) {
+            this.subpage = paths[2];
+          } else {
+            this.subpage = paths[1];
+          }
+          this.loadPage(window.location.href);
+        } else {
+          this.closePage();
         }
-    },
-    openModal(nodeSlug) {
-        console.log("Opening modal for node:", nodeSlug);
-        
-        this.$dispatch('open-modal', { node: nodeSlug });
-
-    }
+        var _this = this;
+        window.addEventListener("popstate", async function () {
+          let pathname = window.location.pathname;
+          let paths = pathname.split("/");
+          if (paths.length > 1 && paths[1]) {
+            if (paths.length > 2) {
+              _this.subpage = paths[2];
+            } else {
+              _this.subpage = paths[1];
+            }
+            _this.loadPage(window.location.href);
+          } else {
+            _this.closePage();
+          }
+        });
+      },
+      updateMap() {
+        this.loadPage(this.$router.path + "?nodes=" + this.$router.query.c)
+      },
+      async loadPage(uid) {
+        let page = await fetch(uid, {
+          headers: {
+            "X-Requested-With": "fetch",
+          },
+        });
+        this.subpage = uid;
+        this.subpagedata = await page.text();
+        try {
+          let tempEl = document.createElement("div");
+          tempEl.innerHTML = this.subpagedata;
+          let title = tempEl.querySelector("[data-title]").dataset.title;
+          tempEl.remove();
+          document.title = title + " - Datacraft";
+    
+        } catch {
+    
+          document.title = "Datacraft";
+        }
+        let subpage = document.querySelector("#subpage");
+        subpage.scrollTop = 0;
+      },
+      closePage() {
+        this.subpage = "home";
+        this.subpagedata = null;
+        this.$router.push("/");
+      },
 }))
 
-Alpine.start()
+Alpine.start();
