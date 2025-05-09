ac72498e99d150747453ca344dc4cd14547d36e4
Amaury Hamon <contact@amauryhamon.com>
2025-05-09 18:14:26 +0200

    conditional rendering for historical wip

--- site/templates/map.php
+++ site/templates/map.php
@@ -9,18 +9,58 @@
                 <a x-link @click="filterMap" x-bind:href="$router.resolve({c:'Project'})" :class="$router.query.c === 'Project' || !$router.query.c ? 'text-green drop-shadow-logo' : 'text-black'">Projects</a>
             </li>
             <li>
-                <a x-link @click="filterMap" x-bind:href="$router.resolve({c:'Historical'})" :class="$router.query.c === 'Historical' || !$router.query.c ? 'text-green drop-shadow-logo' : 'text-black'">Legacy</a>
+                <a x-link @click="filterMap" x-bind:href="$router.resolve({c:'Historical'})" :class="$router.query.c === 'Historical' ? 'text-green drop-shadow-logo' : 'text-black'">Legacy</a>
             </li>
             <li>
-                <a x-link @click="filterMap" x-bind:href="$router.resolve({c:'Recipe'})" :class="$router.query.c === 'Recipe' || !$router.query.c ? 'text-green drop-shadow-logo' : 'text-black'">Recipes</a>
+                <a x-link @click="filterMap" x-bind:href="$router.resolve({c:'Recipe'})" :class="$router.query.c === 'Recipe' ? 'text-green drop-shadow-logo' : 'text-black'">Recipes</a>
             </li>
             
         </ul>
     </div>
     <?php if ($page->children()->isNotEmpty()): ?>
-        <div class="fixed inset-0 w-full h-screen z-40">
+        <div class="<?= $category === 'Historical' ? 'mt-40 relative grid grid-cols-4 gap-6 p-6 z-[40]' : 'fixed inset-0 w-full h-screen z-40' ?>">
 
             <?php foreach ($nodes->values() as $index => $node): ?>
+                <?php if($category === 'Historical'):?>
+                    <a x-link @click="loadPage('<?= $node->url() ?>', true)" :href="window.location.pathname + '?c=Historical'">
+                        <div
+                            class="z-[100] p-2 max-w-96 bg-white text-black shadow-md rounded-md flex gap-2"
+                            x-transition:enter="transition ease-out delay-500 duration-150"
+                            x-transition:enter-start="opacity-0 scale-100"
+                            x-transition:enter-end="opacity-100 scale-100"
+                            x-transition:leave="transition ease-in duration-300"
+                            x-transition:leave-start="opacity-100 scale-100"
+                            x-transition:leave-end="opacity-0 scale-100"
+                        >
+                            <?php if($node->images()->isNotEmpty()):?>
+                                <div class="flex w-48">
+                                    <img class="object-cover aspect-square" src="<?=$node->images()->first()->url()?>" alt="">
+                                </div>
+                            <?php endif?>
+
+                            <div class="p-2 flex flex-col justify-between">
+                                <h4 class="text-2xl/ultratight"><?=$node->title()?></h4>
+                                <div class="text-base flex flex-wrap gap-2">
+                                    <span><?=$node->authors()?></span>
+                                    <span><?=$node->date()?></span>
+                                </div>
+                                <ul class="flex flex-wrap text-xs gap-1">
+                                    <?php if ($node->concept()->isNotEmpty()): ?>
+                                        <?php foreach ($node->concept()->split() as $concept): ?>
+                                            <li class="cardtag"><?= $concept ?></li>
+                                        <?php endforeach ?>
+                                    <?php endif ?>
+                                    <?php if ($node->tools()->isNotEmpty()): ?>
+                                        <?php foreach ($node->tools()->split() as $tool): ?>
+                                            <li class="cardtag"><?= $tool ?></li>
+                                        <?php endforeach ?>
+                                    <?php endif ?>
+                                </ul>
+                            </div>
+                        </div>
+                    </a>
+                <?php else:?>
+                
                 <a
                     href="<?= $node->url() ?>"
                     x-link
@@ -45,8 +85,9 @@
                     }"
                     x-init="setTimeout(() => visible = true, <?= $index ?> * 50)"
                     :class="visible ? 'opacity-100' : 'opacity-0'"
-                    class="select-none absolute flex flex-col justify-center items-center group drop-shadow-logo transition-all duration-700 ease-out"
-                    style="top: <?= $node->ypos()->value() ?>%; left: <?= $node->xpos()->value() ?>%"
+                    class="hover:z-[110] select-none absolute flex flex-col justify-center items-center group drop-shadow-logo transition-all duration-700 ease-out"
+
+                    style="top: <?=$node->ypos()->value()?>%; left: <?=$node->xpos()->value()?>%"
                     @mouseenter.prevent="
                         message = '●';
                         popover = true;"
@@ -55,6 +96,7 @@
                     <h3 class="text-xs max-w-[25ch] text-center mx-auto"><?= $node->title() ?></h3>
                     <?php snippet('popover') ?>
                 </a>
+                <?php endif?>
             <?php endforeach ?>
 
         </div>
