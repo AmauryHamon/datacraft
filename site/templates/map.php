191d71a509d21def7bbdd941a94590d7c0a05a1e
Amaury Hamon <contact@amauryhamon.com>
2025-05-09 10:00:31 +0200

    map

--- site/templates/map.php
+++ site/templates/map.php
@@ -1,7 +1,7 @@
 <?php snippet('head') ?>
 <?php snippet('axis') ?>
 <?php snippet('axis-labels') ?>
-<div x-data="map" x-init="init()" :class="$root.modal ? 'bg-lightgreen' : 'bg-lightgrey'">
+<div>
     <!-- <button class="fixed z-50 top-5 left-1/2 -translate-x-1/2" @click="filterOpen = !filterOpen">Filters</button> -->
     <div class="fixed z-50 top-10 left-1/2 -translate-x-1/2">
         <ul class="flex gap-4">
@@ -11,19 +11,20 @@
             <button class="">…</button>
         </ul>
     </div>
-    
     <?php if ($page->children()->isNotEmpty()): ?>
-        <div class="fixed inset-0 w-full h-screen z-40" >
+        <div class="fixed inset-0 w-full h-screen z-40">
 
             <?php foreach ($page->children()->listed()->values() as $index => $node): ?>
                 <a
                     href="<?= $node->url() ?>"
-                    data-uuid="<?= $node->uuid() ?>"
+                    x-link
+                    @click.prevent="loadPage('<?= $node->url() ?>', true)"
                     x-data="{ 
                         message: '○', 
                         popover: false, 
                         visible: false,
                         node: {
+                            url: '<?= $node->url() ?>',
                             title: '<?= $node->title() ?>',
                             authors: '<?= $node->authors() ?>',
                             date: '<?= $node->date() ?>',
@@ -31,34 +32,29 @@
                             category: '<?= $node->category() ?>',
                             concept: '<?= $node->concept() ?>',
                             tools: '<?= $node->tools() ?>',
-                            uuid: '<?= $node->uuid() ?>',
+                            uid: '<?= $node->uid() ?>',
                             xpos: <?= $node->xpos()->isNotEmpty() ? $node->xpos()->value() : '0' ?>,
                             ypos: <?= $node->ypos()->isNotEmpty() ? $node->ypos()->value() : '0' ?>
                         }
                     }"
                     x-init="setTimeout(() => visible = true, <?= $index ?> * 50)"
                     :class="visible ? 'opacity-100' : 'opacity-0'"
-                    class="absolute flex flex-col justify-center items-center group drop-shadow-logo transition-all duration-700 ease-out"
+                    class="select-none absolute flex flex-col justify-center items-center group drop-shadow-logo transition-all duration-700 ease-out"
                     style="top: <?= $node->ypos()->value() ?>%; left: <?= $node->xpos()->value() ?>%"
                     @mouseenter.prevent="
                         message = '●';
                         popover = true;"
-                        
-                    @mouseleave.prevent="message = '○'; popover = false"
-                    @click.prevent="openModal('<?= $node->slug() ?>')">
-
-
-                    
+                    @mouseleave.prevent="message = '○'; popover = false">
                     <div class="text-base group-hover:text-green" x-text="message">○</div>
                     <h3 class="text-xs max-w-[25ch] text-center mx-auto"><?= $node->title() ?></h3>
-                    <?php snippet('popover')?>
+                    <?php snippet('popover') ?>
                 </a>
             <?php endforeach ?>
 
         </div>
     <?php endif ?>
-
-    <?php snippet('modal') ?>
-    
+    <?php if ($kirby->request()->header('X-Requested-With') === 'fetch'): ?>
+        <?php snippet('modal', ['page' => $page]) ?>
+    <?php endif ?>
 </div>
 <?php snippet('end') ?>
