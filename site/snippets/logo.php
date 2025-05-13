1860bd9052b1728db089b3e51d9bb99198ce0dc5
Amaury Hamon <contact@amauryhamon.com>
2025-05-13 11:33:37 +0200

    no select

--- site/snippets/logo.php
+++ site/snippets/logo.php
@@ -1,9 +1,9 @@
-<div class="fixed z-30 inset-0 w-full h-screen flex flex-col gap-4 items-center justify-center">
+<div class="pointer-events-none fixed z-30 inset-0 w-full h-screen flex flex-col gap-4 items-center justify-center">
     <h1 class="text-lightgreen text-[15vw] font-display select-none drop-shadow-logo leading-[0.7] mb-8"
     :class="subpage === 'home' ? 'opacity-100' : 'opacity-20'"
     >datacraft</h1>
     <?php if ($showParagraph ?? false): ?>
-        <p x-show="subpage === 'home'"class="mt-16 absolute top-1/2 text-center text-xs sm:text-base">
+        <p x-show="subpage === 'home'"class="select-none mt-16 absolute top-1/2 text-center text-xs sm:text-base">
             <?=$site->tagline()?>
         </p>
     <?php endif; ?>
