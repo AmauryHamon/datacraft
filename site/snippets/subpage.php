bd61345364161706bed018c6b9a1d05b3479c059
Amaury Hamon <contact@amauryhamon.com>
2025-05-13 11:33:43 +0200

    update zindex

--- site/snippets/subpage.php
+++ site/snippets/subpage.php
@@ -2,7 +2,7 @@
       id="subpage" 
       x-transition.opacity.duration.500ms 
       x-show="subpage !== 'home'" 
-      class="w-full absolute z-40 top-0 left-0 min-h-[calc(100vh-96px)]  bg-opacity-95 pb-20 sm:pb-0"
+      class="w-full absolute z-[110] top-0 left-0 min-h-[calc(100vh-96px)]  bg-opacity-95 pb-20 sm:pb-0"
 
 >
       <div class="overflow-y-scroll" x-show="subpage && subpage.length > 0" x-html="subpagedata" ></div>
